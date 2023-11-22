89047bcc0db3881a25a19ee2ec46ea2e142739d1
Billy <billychorey@gmail.com>
2023-11-21 20:03:39 -0500

    more style changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Item({ title, image, price, quantity }) {
   return (
     <div>
-      <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <span className='bold'>${price}</span>
-      <p className='quantity'>Quantity in stock: {quantity}</p>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <span className='bold'>${price}</span>
+        <p className='quantity'>Quantity in stock: {quantity}</p>
     </div>
   );
 }
